a4b4dccd6398ac0886350d5f6febf64cf7d3bb78
Aiswarya <aiswarya.aisuu@gmail.com>
2024-01-25 20:09:54 +0530

    My First Portfolio Project

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+
+from blog.models import Blog
+
+
+# Create your views here.
+def all_blogs(request):
+    # blog_count = Blog.objects.count
+    # blogs = Blog.objects.order_by('-date')[:5] # to get the recent 5 blogs only
+    blogs = Blog.objects.order_by('-date')
+    return render(request, 'blog/all_blogs.html', {'blogs':blogs})
+
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id) # pk - primary key
+    # return render(request, 'blog/detail.html', {'id':blog_id})
+    return render(request, 'blog/detail.html', {'blog':blog})
